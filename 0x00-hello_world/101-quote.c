3f2ded43263fa548b3963cb7652419281f43c1a3
Titime57 <fatimaezzahraa355@gmail.com>
2023-04-20 15:19:03 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - A program that prints a line to the standard error
+ * main - Entry point
  * Return: 1 (success)
  */
 int main(void)
 {
-	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
