f6f8fe54fa4a92f37d11d2869c330272ded9fe69
Titime57 <fatimaezzahraa355@gmail.com>
2023-05-18 08:21:15 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,92 +2,33 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
- * is_digit - checks if a string contains a non-digit char
- * @s: string to be evaluated
- * Return: 0 if a non-digit is found, 1 if it is found
- */
-int is_digit(char *s)
-{
-	int l = 0;
-
-	while (s[l])
-	{
-		if (s[l] < '0' || s[l] > '9')
-			return (0);
-		l++;
-	}
-	return (1);
-}
-/**
- * _strlen - return the lenght of a string
- * @s: string to evaluate
- * Return: the length of a string
- */
-int _strlen(char *s)
-{
-	int l = 0;
-
-	while (s[l] != '\0')
-	{
-		l++;
-	}
-	return (l);
-}
-/**
- * errors - handles errors for main
- */
-void errors(void)
-{
-	printf("ERROR\n");
-	exit(98);
-}
-/**
  * main - multiplies two positive numbers
  * @argc: number of arguments
  * @argv: array of arguments
- * Return: 0
+ * Return: int
  */
 int main(int argc, char *argv[])
 {
-	char *s1, *s2;
-	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
+	unsigned long mul;
+	int p, q;
 
-	s1 = argv[1], s2 = argv[2];
-	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
-		errors();
-	len1 = _strlen(s1);
-	len2 = _strlen(s2);
-	len = len1 + len2 + 1;
-	result = malloc(sizeof(int) * len);
-	if (!result)
-		return (1);
-	for (i = 0; i <= len1 + len2; i++)
-		result[i] = 0;
-	for (len1 = len1 - 1; len1 >= 0; len1--)
+	if (argc != 3)
 	{
-		digit1 = s1[len1] - '0';
-		carry = 0;
-		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
+		printf("Error\n");
+		exit(98);
+	}
+	for (p = 1; p < argc; p++)
+	{
+		for (q = 0; argv[p][q] != '\0'; q++)
 		{
-			digit2 = s2[len2] - '0';
-			carry += result[len1 + len2 + 1] + (digit1 * digit2);
-			result[len1 + len2 + 1] = carry % 10;
-			carry /= 10;
+			if (argv[p][q] > 57 || argv[p][q] < 48)
+			{
+				printf("Error\n");
+				exit(98);
+			}
 		}
-		if (carry > 0)
-			result[len1 + len2 + 1] += carry;
 	}
-	for (i = 0; i < len - 1; i++)
-	{
-		if (result[i])
-			a = 1;
-		if (a)
-			_putchar(result[i] + '0');
-	}
-	if (!a)
-		_putchar('0');
-	_putchar('\n');
-	free(result);
+	mul = atol(argv[1]) * atol(argv[2]);
+	printf("%lu\n", mul);
 	return (0);
 }
-
