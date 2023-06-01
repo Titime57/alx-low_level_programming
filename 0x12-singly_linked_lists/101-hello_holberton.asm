c8066d9a2d6110a27fd7046905f411ab4a8da06c
Titime57 <fatimaezzahraa355@gmail.com>
2023-06-01 14:52:54 -0700

    101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,9 +1,8 @@
 extern printf
 global main
 main:
-mov edi, format
-xor eax, eax
+mov edi, msg
+mov eax, 0
 call printf
 mov eax,0
-ret
-format: db 'Hello, Holberton\n',0
+msg: db 'Hello, Holberton',0xa, 0
