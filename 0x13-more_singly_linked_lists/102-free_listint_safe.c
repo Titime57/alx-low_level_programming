7e51776a8d2c4ba7e0d4a4ab373d51eb205878f9
Titime57 <fatimaezzahraa355@gmail.com>
2023-06-06 12:01:00 -0700

    101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,4 +1,44 @@
 #include "lists.h"
+size_t looped_listint_len(const listint_t *head);
+size_t print_listint_safe(const listint_t *head);
+/**
+ * looped_listint_len - counts the number of unique nodes
+ * @head: a pointer to the head of the listint_t to check
+ * Return: the number of unique nodes in the list, otherwise 0
+ */
+size_t looped_listint_len(const listint_t *head)
+{
+        const listint_t *top, *h;
+        size_t nodes = 1;
+
+        if (head == NULL || head->next == NULL)
+                return (0);
+        top = head->next;
+        h = (head->next)->next;
+        while (h)
+        {
+                if (top == h)
+                {
+                        top = head;
+                        while (top != h)
+                        {
+                                nodes++;
+                                top = top->next;
+                                h = h->next;
+                        }
+                        top = top->next;
+                        while (top != h)
+                        {
+                                nodes++;
+                                top = top->next;
+                        }
+                        return (nodes);
+                }
+                top = top->next;
+                h = (h->next)->next;
+        }
+        return (0);
+}
 /**
  * free_listint_safe - frees a linked list
  * @h: pointer to the first node in the linked list
