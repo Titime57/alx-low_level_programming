cf37d926110c2323064a6f73a7645d238c182f00
Titime57 <fatimaezzahraa355@gmail.com>
2023-06-12 16:35:46 -0700

    100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,11 +1,11 @@
 #include "main.h"
 #include <elf.h>
-void print_osabi_more(ELF64_Ehdr h);
+void print_osabi_more(Elf64_Ehdr h);
 /**
  * print_magic - prints ELF magic bytes
  * @h: the ELF header struct
  */
-void print_magic(ELF64_Ehdr h)
+void print_magic(Elf64_Ehdr h)
 {
 	int i;
 
@@ -14,3 +14,12 @@ void print_magic(ELF64_Ehdr h)
 		printf("%2.2x%s", h.e_ident[i],
 				i == EI_NIDENT - 1 ? "\n" : " ");
 }
+/**
+ * print_class - prints ELF class
+ * @h: the ELF header struct
+ */
+void print_class(Elf64_Ehdr h)
+{
+	printf("  Class:                           ");
+	switch (h.e_ident[EI_CLASS])
+	{
