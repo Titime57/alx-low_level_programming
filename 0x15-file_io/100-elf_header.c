91ae20fbd02a3f2549369077bd29344c83d95ad2
Titime57 <fatimaezzahraa355@gmail.com>
2023-06-12 18:07:22 -0700

    100-elf_header.c

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -39,7 +39,7 @@ void print_class(Elf64_Ehdr h)
 * print_data - prints ELF data
 * @h: the ELF header struct
 */
-void print_data - (Elf64_Ehdr h)
+void print_data(Elf64_Ehdr h)
 {
 	printf("  Data:						");
 	switch (h.e_ident[EI_DATA])
@@ -159,6 +159,7 @@ void print_type(Elf64_Ehdr h)
 {
 	char *p = (char *)&h.e_type;
 	int i = 0;
+
 	printf("  Type:					");
 	if (h.e_ident[EI_DATA] == ELFSATA2MSB)
 		i = 1;
@@ -215,5 +216,49 @@ void print_entry(Elf64_Ehdr h)
 			printf("%02x", p[i]);
 		printf("\n");
 	}
+}
+/**
+ * main - program
+ * @ac: argument count
+ * @av: argument vector
+ * Return: 1 (sucess), 0 (fail)
+ */
+int main(int ac, char **av)
+{
+	int fd;
+	Elf64_Ehdr h;
+	ssize_t b;
+
+	if (ac != 2)
+		dprintf(STDERR_FILENO, "Usage: elf_header elf_filename\n")
+			exit(98);
+	fd = open(av[1], O_RDONLY);
+	if (fd == -1)
+		dprintf(STDERR_FILENO, "Can't open file: %s\n", av[1])
+			exit(98);
+	b = read(fd, &h, sizeof(h));
+	if (b < 1 || b != sizeof(h))
+		dprintf(STDERR_FILENO, "Can't read from file: %s\n", av[1])
+			exit(98);
+	if (h.e_ident[0] == 0x7f && h.e_ident[1] == 'E' &&
+			h.e_ident[2] == 'L' && h.e_ident[3] == 'F')
+	{
+		printf("ELF Header:\n");
+	}
+	else
+		dprintf(STDERR_FILENO, "Not ELF file: %s\n", av[1])
+			exit(98);
+	print_magic(h);
+	print_class(h);
+	print_data(h);
+	print_version(h);
+	print_osabi(h);
+	print_abiversion(h);
+	print_type(h);
+	print_entry(h);
+	if (close(fd))
+		dprintf(STDERR_FILENO, "Error closing file descriptor: %d\n", fd)
+			exit(98);
+	return (EXIT_SUCCESS);
 }
 
